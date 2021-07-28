37707842f9e848b5d12e41f828b67d3d76d9cc9b
PSeskauskas <patrikas.seskauskas@ucdconnect.ie>
2021-07-28 11:51:59 +0100

    Implemented game board image

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -1,7 +1,21 @@
 package com.company;
 
-public class Board {
-    public static void main(String[] args) {
-        System.out.println("Board!");
+import javafx.scene.Parent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Board extends Parent {
+    public static ImageView loadImage() throws FileNotFoundException {
+        Image image = new Image(new FileInputStream("600px-Ludo_board.svg.png"));
+        ImageView imageView = new ImageView(image);
+        imageView.setX(0);
+        imageView.setY(0);
+        imageView.setFitHeight(800);
+        imageView.setFitWidth(800);
+        imageView.setPreserveRatio(true);
+        return imageView;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,24 @@
 package com.company;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
-	    System.out.println("Hello World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Ludo Game");
+        primaryStage.setResizable(false);
+
+        Group root = new Group(Board.loadImage());
+
+        Scene scene = new Scene(root, 800, 800);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
