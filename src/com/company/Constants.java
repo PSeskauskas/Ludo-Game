c80fc47aff6f87a5e3822a95d6973e3261e09f3d
RBo00 <rorybrowne11@gmail.com>
2021-07-29 11:23:26 +0100

    added game constants

---
+++ src/com/company/Constants.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Constants {
+    private static final int  NUM_PLAYERS = 4;
+
+    private static final int MAIN_SQUARES = 52;
+
+    private static final int NUM_MID_SQUARES =7;
+
+    public enum COLOURS {BlUE, GREEN, YELLOW, RED}
+
+    private static final int PLAYER_PIECES = 4;
+
+}
---
+++ src/com/company/Player.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Player {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+}
