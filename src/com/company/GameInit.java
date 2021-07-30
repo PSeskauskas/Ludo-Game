95a49577efa150ff3c29d4d5ff7e3c7fd69d9940
PSeskauskas <patrikas.seskauskas@ucdconnect.ie>
2021-07-30 12:23:45 +0100

    Added a window to allow selection for number of players

---
+++ src/com/company/GameInit.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class GameInit {
+
+    static int choice;
+
+    public static int selectPlayers() {
+        Stage newWindow = new Stage();
+        newWindow.setWidth(300);
+        newWindow.setHeight(300);
+        newWindow.setTitle("Please select how many players are playing");
+        newWindow.setResizable(false);
+        Label label = new Label("Select here");
+
+        ComboBox select = new ComboBox();
+        select.getItems().add(2);
+        select.getItems().add(3);
+        select.getItems().add(4);
+
+        Button submit = new Button("Submit");
+        submit.setOnAction(e -> {
+            choice = (int) select.getValue();
+            newWindow.close();
+        });
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label, select, submit);
+        layout.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(layout);
+        newWindow.setScene(scene);
+        newWindow.showAndWait();
+        return choice;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -13,6 +14,9 @@ import javafx.stage.Popup;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+    static int numPlayers;
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -22,16 +26,11 @@ public class Main extends Application {
         primaryStage.setTitle("Ludo Game");
         primaryStage.setResizable(false);
 
-        final Popup popup = new Popup();
-        popup.setX(300);
-        popup.setY(200);
-        popup.getContent().addAll(new Circle(25, 25, 50, Color.AQUAMARINE));
-
-        Button show = new Button("Show");
-        show.setOnAction(event -> popup.show(primaryStage));
-
-        Button hide = new Button("Hide");
-        hide.setOnAction(event -> popup.hide());
+        Button startGame = new Button("Start Game");
+        startGame.setOnAction(event -> {
+            numPlayers = GameInit.selectPlayers();
+            startGame.setDisable(true);
+        });
 
         Group root = new Group(Board.loadImage());
         for(int i = 0; i < Constants.PLAYER_PIECES * Constants.NUM_PLAYERS; i++) {
@@ -45,10 +44,9 @@ public class Main extends Application {
                 root.getChildren().add(Piece.loadPiece(Constants.STARTING_POS[0][i], Constants.STARTING_POS[1][i], 10, i, Constants.COLOURS.BLUE));
             }
         }
-        VBox vbox = new VBox(5);
-        vbox.getChildren().addAll(show, hide);
-
-        root.getChildren().add(vbox);
+        if(numPlayers == 0) {
+            root.getChildren().add(startGame);
+        }
         root.setScaleY(0.88);
 
         Scene scene = new Scene(root, 800, 800);
