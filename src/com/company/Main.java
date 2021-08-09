153404e70c4ac7aab58abc63a8bece413f6471e7
PSeskauskas <patrikas.seskauskas@ucdconnect.ie>
2021-08-09 12:04:04 +0100

    Attempted to implement moving every piece to its starting square

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
     static int count;
     static Square[] board = new Square[52];
     static Circle[] circles = new Circle[16];
+    static Boolean[] inPlay = new Boolean[16];
 
     public static void main(String[] args) {
         int i = 0;
@@ -139,20 +140,44 @@ public class Main extends Application {
             if(res == 6) {
                 listView.getItems().add(players[count].getName() + " has rolled a " + res + ". A piece has been brought out");
                 if(players[count].getColor() == Color.GREEN) {
-                    circles[0].setCenterX(board[1].getX_coord());
-                    circles[0].setCenterY(board[1].getY_cord());
+                    for(int i = 0; i < Constants.PLAYER_PIECES; i++) {
+                        if(!inPlay[i]) {
+                            circles[i].setCenterX(board[1].getX_coord());
+                            circles[i].setCenterY(board[1].getY_cord());
+                            inPlay[i] = true;
+                            break;
+                        }
+                    }
                 }
                 if(players[count].getColor() == Color.RED) {
-                    circles[8].setCenterX(board[14].getX_coord());
-                    circles[8].setCenterY(board[14].getY_cord());
+                    for(int i = 8; i < Constants.PLAYER_PIECES * 3; i++) {
+                        if(!inPlay[i]) {
+                            circles[i].setCenterX(board[14].getX_coord());
+                            circles[i].setCenterY(board[14].getY_cord());
+                            inPlay[i] = true;
+                            break;
+                        }
+                    }
                 }
                 if(players[count].getColor() == Color.BLUE) {
-                    circles[12].setCenterX(board[27].getX_coord());
-                    circles[12].setCenterY(board[27].getY_cord());
+                    for(int i = 12; i < Constants.PLAYER_PIECES * 4; i++) {
+                        if(!inPlay[i]) {
+                            circles[i].setCenterX(board[27].getX_coord());
+                            circles[i].setCenterY(board[27].getY_cord());
+                            inPlay[i] = true;
+                            break;
+                        }
+                    }
                 }
                 if(players[count].getColor() == Color.YELLOW) {
-                    circles[4].setCenterX(board[40].getX_coord());
-                    circles[4].setCenterY(board[40].getY_cord());
+                    for(int i = 4; i < Constants.PLAYER_PIECES * 2; i++) {
+                        if(!inPlay[i]) {
+                            circles[i].setCenterX(board[40].getX_coord());
+                            circles[i].setCenterY(board[40].getY_cord());
+                            inPlay[i] = true;
+                            break;
+                        }
+                    }
                 }
             } else if(res != 6) {
                 listView.getItems().add(players[count].getName() + " has rolled a " + res + ".");
