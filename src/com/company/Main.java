346fb3fbe4273d736e288fef24d3c388c8637f90
PSeskauskas <patrikas.seskauskas@ucdconnect.ie>
2021-09-03 12:09:00 +0100

    Fixed winner condition

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -167,6 +167,7 @@ public class Main extends Application {
                     layout.setAlignment(Pos.CENTER);
                     Scene scene = new Scene(layout);
                     winnerWindow.setScene(scene);
+                    winnerWindow.show();
                 }
             }
             if (count < numPlayers && res != 6) {
